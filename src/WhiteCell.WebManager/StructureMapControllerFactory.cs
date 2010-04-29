07091074cc12ddb5a803ecc6f0e78089b994086c
Emil Cardell <emil.cardell@gmail.com>
2010-04-29 22:29:57 +0200

    First commit. Added spark and structuremap to WebManager. Added ravendb.

---
+++ src/WhiteCell.WebManager/StructureMapControllerFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using StructureMap;
+
+namespace WhiteCell.WebManager
+{
+    public class StructureMapControllerFactory : DefaultControllerFactory
+    {
+        protected override IController GetControllerInstance(System.Web.Routing.RequestContext requestContext, Type controllerType)
+        {
+            if (controllerType == null)
+                return null;
+            return (IController)ObjectFactory.GetInstance(controllerType);
+        }
+    }
+}
